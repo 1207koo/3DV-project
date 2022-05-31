143f95a5bd80f4fff5be8a533b0fac32eb5370c5
1207koo <1207koo@snu.ac.kr>
2022-05-31 18:46:34 +0900

    added d2-net

---
+++ d2-net/download_model.sh
@@ -0,0 +1,4 @@
+mkdir models
+wget https://dsmn.ml/files/d2-net/d2_ots.pth -O models/d2_ots.pth --no-check-certificate
+wget https://dsmn.ml/files/d2-net/d2_tf.pth -O models/d2_tf.pth --no-check-certificate
+wget https://dsmn.ml/files/d2-net/d2_tf_no_phototourism.pth -O models/d2_tf_no_phototourism.pth --no-check-certificate
