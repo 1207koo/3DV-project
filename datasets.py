2aaffe8f25708d35f7b8b59e982559820948a79f
1207koo <1207koo@snu.ac.kr>
2022-06-04 00:28:19 +0900

    img size args for dataset

--- datasets.py
+++ datasets.py
@@ -4,6 +4,6 @@ from args import *
 
 def get_dataset(mode='train'):
     if args.dataset == 'COCO2014':
-        return COCODataset(os.path.join(args.dataset_path, args.dataset), use=['%s2014'%mode], transform=[StrongCrop(256), ColorNormal()])
+        return COCODataset(os.path.join(args.dataset_path, args.dataset), use=['%s2014'%mode], transform=[StrongCrop(args.img_size), ColorNormal()])
     else:
         raise NotImplementedError
